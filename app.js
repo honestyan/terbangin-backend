1909e253cd145ff968ae3aa755d8cfafe15dec07
honestyan <honestyan0708@gmail.com>
2022-12-08 04:52:28 +0700

    fix:cors policy

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const router = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 const { PORT } = process.env;
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(router);
 app.use((req, res, next) => {
   return res.status(404).json({
