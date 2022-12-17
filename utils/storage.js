abfcbd1885e93bc360fae4d1dc9ce881cc3dd549
honestyan <honestyan0708@gmail.com>
2022-12-17 22:19:31 +0700

    feat:add deleteFile on multer

--- utils/storage.js
+++ utils/storage.js
@@ -1,41 +1,40 @@
-const multer = require('multer');
-const path = require('path');
-
-
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, './public/document');
-    },
+  destination: (req, file, callback) => {
+    callback(null, "./public/document");
+  },
 
-    // generete unique filename
-    filename: (req, file, callback) => {
-        const namaFile = Date.now() + path.extname(file.originalname);
-        filename = namaFile;
-        callback(null, namaFile);
-    }
+  filename: (req, file, callback) => {
+    const namaFile = Date.now() + path.extname(file.originalname);
+    filename = namaFile;
+    callback(null, namaFile);
+  },
 });
 
-
 module.exports = {
-    document: multer({
-        storage: storage,
-        fileFilter: (req, file, callback) => {
-            console.log("ini dari multer : ".file)
-            console.log(file.mimetype)
-            if (file.mimetype == 'application/pdf' ) {
-                console.log(file)
-                callback(null, true);
-            } else {
-                const err = new Error('only pdf allowed to upload!');
-                return callback(err, false);
-            }
-        },
-
-        // error handling
-        onError: (err, next) => {
-            next(err);
-        }
-    })
-
+  deleteFile: (filename) => {
+    fs.unlink(`./public/document/${filename}`, (err) => {
+      if (err) {
+        next(err);
+      }
+    });
+  },
+  document: multer({
+    storage: storage,
+    fileFilter: (req, file, callback) => {
+      if (file.mimetype == "application/pdf") {
+        callback(null, true);
+      } else {
+        const err = new Error("only pdf allowed to upload!");
+        return callback(err, false);
+      }
+    },
+    // error handling
+    onError: (err, next) => {
+      next(err);
+    },
+  }),
 };
