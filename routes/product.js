6cb20177c1ae8d9ae760a5cd74cdb27496c6c2b6
honestyan <honestyan0708@gmail.com>
2022-12-26 21:04:32 +0700

    remove: authorization product

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("../controllers");
 const mid = require("../helpers/middleware");
 
-router.get("/", mid.mustLogin, controller.product.getAll);
-router.get("/:id", mid.mustLogin, controller.product.getOne);
+router.get("/", controller.product.getAll);
+router.get("/:id", controller.product.getOne);
 
 module.exports = router;
